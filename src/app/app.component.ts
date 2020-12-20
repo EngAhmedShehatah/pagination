ca01ff4c6cc00378a8772f268042c245dec10927
EngAhmedShehatah <engahmedshehatah@gmail.com>
2020-12-20 14:33:06 -0800

    Stephen Grider's 5th app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,107 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1472074468533-c2cb83695b0e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80'
+    },
+    {
+      title: 'While Coding',
+      url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+    },
+    {
+      title: 'enjoying Baking',
+      url: 'https://images.unsplash.com/photo-1514411959691-a8f39b0ac8b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1410&q=80'
+    },
+    {
+      title: 'at Work',
+      url: 'https://images.unsplash.com/photo-1580920462192-3d40a3bb7bc2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1472074468533-c2cb83695b0e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80'
+    },
+    {
+      title: 'While Coding',
+      url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+    },
+    {
+      title: 'enjoying Baking',
+      url: 'https://images.unsplash.com/photo-1514411959691-a8f39b0ac8b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1410&q=80'
+    },
+    {
+      title: 'at Work',
+      url: 'https://images.unsplash.com/photo-1580920462192-3d40a3bb7bc2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1472074468533-c2cb83695b0e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80'
+    },
+    {
+      title: 'While Coding',
+      url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+    },
+    {
+      title: 'enjoying Baking',
+      url: 'https://images.unsplash.com/photo-1514411959691-a8f39b0ac8b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1410&q=80'
+    },
+    {
+      title: 'at Work',
+      url: 'https://images.unsplash.com/photo-1580920462192-3d40a3bb7bc2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1472074468533-c2cb83695b0e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80'
+    },
+    {
+      title: 'While Coding',
+      url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+    },
+    {
+      title: 'enjoying Baking',
+      url: 'https://images.unsplash.com/photo-1514411959691-a8f39b0ac8b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1410&q=80'
+    },
+    {
+      title: 'at Work',
+      url: 'https://images.unsplash.com/photo-1580920462192-3d40a3bb7bc2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1472074468533-c2cb83695b0e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80'
+    },
+    {
+      title: 'While Coding',
+      url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+    },
+    {
+      title: 'enjoying Baking',
+      url: 'https://images.unsplash.com/photo-1514411959691-a8f39b0ac8b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1410&q=80'
+    },
+    {
+      title: 'at Work',
+      url: 'https://images.unsplash.com/photo-1580920462192-3d40a3bb7bc2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1472074468533-c2cb83695b0e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80'
+    },
+    {
+      title: 'While Coding',
+      url: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+    },
+    {
+      title: 'enjoying Baking',
+      url: 'https://images.unsplash.com/photo-1514411959691-a8f39b0ac8b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1410&q=80'
+    },
+    {
+      title: 'at Work',
+      url: 'https://images.unsplash.com/photo-1580920462192-3d40a3bb7bc2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'
+    }
+  ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
